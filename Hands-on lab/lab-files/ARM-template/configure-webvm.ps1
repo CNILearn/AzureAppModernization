28e7ce5f654d245adfaa01885b6351ae3462e30c
Christian Nagel <christian@christiannagel.com>
2023-11-25 17:35:35 +0100

    change edge link

--- Hands-on lab/lab-files/ARM-template/configure-webvm.ps1
+++ Hands-on lab/lab-files/ARM-template/configure-webvm.ps1
@@ -69,7 +69,7 @@ Write-Host "Server=$SqlIP;Database=PartsUnlimited;User Id=PUWebSite;Password=$Sq
 # Download App Service Migration Assistant 
 (New-Object System.Net.WebClient).DownloadFile('https://appmigration.microsoft.com/api/download/windows/AppServiceMigrationAssistant.msi', 'C:\AppServiceMigrationAssistant.msi')
 # Download Edge 
-(New-Object System.Net.WebClient).DownloadFile('https://go.microsoft.com/fwlink/?LinkID=2093437', 'C:\MicrosoftEdgeEnterpriseX64.msi')
+(New-Object System.Net.WebClient).DownloadFile('https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/e23c074b-1771-4f3a-91b6-5cb01e793912/MicrosoftEdgeEnterpriseX64.msi', 'C:\MicrosoftEdgeEnterpriseX64.msi')
 # Download 3.1.4 SDK
 (New-Object System.Net.WebClient).DownloadFile('https://download.visualstudio.microsoft.com/download/pr/70062b11-491c-403c-91db-9d84462ee292/5db435e39128cbb608e76bf5111ab3dc/dotnet-sdk-3.1.413-win-x64.exe', 'C:\dotnet-sdk-3.1.413-win-x64.exe')
 
